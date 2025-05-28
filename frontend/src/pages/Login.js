c83c56bb4cf975267b7bd5262f1a78bcf228e806
ASLAM <34620180+aslambeest@users.noreply.github.com>
2025-05-28 14:37:34 -0700

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ export default function LoginPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', form);
+      const res = await axios.post('https://connectingnepali.onrender.com/api/auth/login', form);
       localStorage.setItem('token', res.data.token);
       setMessage('Login successful!');
       navigate('/dashboard');
